b376c2f8a0b7b63d97d4aa5720b28cbe101037b6
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-01 19:49:39 -0600

    validación de email unique v2

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { BadRequest } from 'http-errors';
 import models from '../database/models';
 
 export class AuthService {
@@ -14,6 +15,10 @@ export class AuthService {
 
   //register user
   async registerUser(user: any) {
+    const userDB = await models.User.findOne({ email: user.email });
+    if (userDB) {
+      throw new BadRequest('The email already exists, try again!');
+    }
     const registeredUser = await models.User.create(user);
     return registeredUser;
   }
